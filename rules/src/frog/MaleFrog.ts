8987c878ed3d0569704f43409fffef12161f24e4
sulfab <p.beaume@gmail.com>
2021-03-29 11:44:46 +0200

    fix: change MaleFrog enum to numeric

--- rules/src/frog/MaleFrog.ts
+++ rules/src/frog/MaleFrog.ts
@@ -1,8 +1,8 @@
 export enum MaleFrog {
-    Red = 'Red',
-    Blue = 'Blue',
-    Pink = 'Pink',
-    Yellow = 'Yellow',
-    Purple = 'Purple',
-    Green = 'Green'
+    Red,
+    Blue,
+    Pink,
+    Yellow,
+    Purple,
+    Green
 }
