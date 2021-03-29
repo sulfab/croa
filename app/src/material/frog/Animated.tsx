126abe5eeccfa3d00ffd9d8dc424381c00c1f8af
sulfab <p.beaume@gmail.com>
2021-03-29 11:45:37 +0200

    feat: compute the keyframe to use the delay time

--- app/src/material/frog/Animated.tsx
+++ app/src/material/frog/Animated.tsx
@@ -16,17 +16,18 @@ type AnimatedProps = {
 
 const Animated: FC<AnimatedProps> = ({ animation, image, frame, visible, duration, delay, loop, color, ...props }) => {
 
+    const delayPercentage = (!delay || !duration)? 0: delay / (delay + duration) * 100;
     return (
-        <div { ...props } css={ [container(frame, visible), duration && getAnimation(frame, duration, delay, loop) ]} style={{ backgroundImage: `url(${image})` }} />
+        <div { ...props } css={ [container(frame, visible), duration && getAnimation(frame, duration, delay, delayPercentage, loop) ]} style={{ backgroundImage: `url(${image})` }} />
     );
 };
 
-const animationKeyFrame = (frames: number) => keyframes`
-    from { 
-        background-position: 0;
-    }
-    to { 
-        background-position: -${frames * 100}%;
+
+const animationKeyFrame = (frame: number, delayPercentage?: number) => keyframes`
+    0% { background-position: 0; }
+    ${delayPercentage? `${delayPercentage}% { background-position: 0; }`: ''}
+    100% { 
+        background-position: -${frame * 100}%;
     }
 `
 
@@ -38,8 +39,8 @@ const container = (frame: number, visible?: boolean) => css`
     ${ !visible? `width: 0% !important;`: '' };
 `
 
-const getAnimation = (frames: number, duration?: number, delay?: number, loop?: boolean) => css`
-    animation: ${animationKeyFrame(frames)} ${ duration }s steps(${frames}) ${delay || 0}s ${loop ? 'infinite': 1};
+const getAnimation = (frames: number, duration?: number, delay?: number, delayPercentage?: number, loop?: boolean) => css`
+    animation: ${animationKeyFrame(frames, delayPercentage)} ${ (delay || 0) + (duration || 0) }s steps(${frames}) ${loop ? 'infinite': 1};
 `
 export {
     Animated
