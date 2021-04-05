aa1b3a789f3539591808f646b79335a8d5a3ff48
sulfab <p.beaume@gmail.com>
2021-04-06 01:09:08 +0200

    feat: add welcome popup

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,16 +1,17 @@
-import { css, keyframes } from '@emotion/react';
-import { GameState } from '@gamepark/croa/GameState';
-import { usePlayerId } from '@gamepark/react-client';
-import { Letterbox } from '@gamepark/react-components';
-import { Board } from './material/board/Board';
-import { PlayerBoard } from './material/player/PlayerBoard';
-import { PlayerBoardPlacement } from './material/player/PlayerBoardPlacement';
-import { Ranking } from './ranking/Ranking';
-import { playerBoardHeight, playerWidth } from './utils/Styles';
-import React from 'react';
-import { SlabDescription } from './material/tile/description/SlabDescription';
-import { SlabDescriptionVisual } from './material/tile/description/SlabDescriptionVisual';
+import {css, keyframes} from '@emotion/react';
+import {GameState} from '@gamepark/croa/GameState';
+import {usePlayerId} from '@gamepark/react-client';
+import {Letterbox} from '@gamepark/react-components';
+import {Board} from './material/board/Board';
+import {PlayerBoard} from './material/player/PlayerBoard';
+import {PlayerBoardPlacement} from './material/player/PlayerBoardPlacement';
+import {Ranking} from './ranking/Ranking';
+import {playerBoardHeight, playerWidth} from './utils/Styles';
+import React, {useState} from 'react';
+import {SlabDescription} from './material/tile/description/SlabDescription';
+import {SlabDescriptionVisual} from './material/tile/description/SlabDescriptionVisual';
 import TutorialPopup from './tutorial/TutorialPopup';
+import WelcomePopup from './welcome/WelcomePopup';
 
 type Props = {
   game: GameState
@@ -20,6 +21,9 @@ const GameDisplay: React.FC<Props> = ({game}: Props) => {
   const playerId = usePlayerId();
   const playerIndex = game.players.findIndex(player => player.color === playerId) === -1? 0: game.players.findIndex(player => player.color === playerId);
   const getPlayer = (index: number) => game.players.find((_, i) => PlayerBoardPlacement[game.players.length].getPlayerBoard(i, playerIndex) === index);
+  const player = game.players.find(p => p.color === playerId);
+  const [welcomePopupClosed, setWelcomePopupClosed] = useState(game.pond.some(s => s.some(s2 => s2.displayed)))
+  const displayWelcome = !game.tutorial && !welcomePopupClosed
 
   return (
     <>
@@ -28,18 +32,19 @@ const GameDisplay: React.FC<Props> = ({game}: Props) => {
         <div css={ gameBoard }>
           <Board pond={ game.pond } playerColors={ game.players.map(p => p.color )} frogs={ game.players.flatMap(p => p.femaleFrogs.filter(frog => !!frog.position)) } activePlayer={ game.players.find(player => player.color === game.activePlayer) } playerIndex={ playerIndex } playerCount={ game.players.length }/>
           <div css={[ playerBoards, leftPlayerBoards ]}>
-            { getPlayer(0) && <PlayerBoard index={0} css={ css`bottom: 0%`} player={ getPlayer(0)! } activePlayer={ game.activePlayer } /> }
+            { getPlayer(0) && <PlayerBoard index={0} css={ css`bottom: 0`} player={ getPlayer(0)! } activePlayer={ game.activePlayer } /> }
             <SlabDescriptionVisual css={ lastSlabImage } />
-            { getPlayer(1) && <PlayerBoard index={1} css={ css`top: 0%`}  player={ getPlayer(1)! } activePlayer={ game.activePlayer } /> }
+            { getPlayer(1) && <PlayerBoard index={1} css={ css`top: 0`} player={ getPlayer(1)! } activePlayer={ game.activePlayer } /> }
           
           </div>
           <div css={[ playerBoards, rightPlayerBoards ]}>
-            { getPlayer(2) && <PlayerBoard index={2} css={ css`top: 0%`} player={ getPlayer(2)! } activePlayer={ game.activePlayer }  /> }
+            { getPlayer(2) && <PlayerBoard index={2} css={ css`top: 0`} player={ getPlayer(2)! } activePlayer={ game.activePlayer }  /> }
             <SlabDescription css={ lastFrontSlabDescription } />
-            { getPlayer(3) && <PlayerBoard index={3} css={ css`bottom: 0%`} player={ getPlayer(3)! } activePlayer={ game.activePlayer } /> }
+            { getPlayer(3) && <PlayerBoard index={3} css={ css`bottom: 0`} player={ getPlayer(3)! } activePlayer={ game.activePlayer } /> }
           </div>
         </div>
         {game.tutorial && <TutorialPopup game={game}/>}
+        { displayWelcome && player && <WelcomePopup player={player} close={() => setWelcomePopupClosed(true)}/>}
       </Letterbox>
     </>
   )
---
+++ app/src/welcome/WelcomePopup.tsx
@@ -0,0 +1,68 @@
+import {css} from '@emotion/react';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {usePlayer} from '@gamepark/react-client';
+import {FunctionComponent} from 'react';
+import {useTranslation} from 'react-i18next';
+import {
+    closePopupStyle,
+    popupLightStyle,
+    popupOverlayStyle,
+    popupPosition,
+    popupStyle,
+    showPopupOverlayStyle
+} from '../utils/Styles';
+import {FrogAnimation} from '../material/frog/FrogAnimation';
+import {Player} from '@gamepark/croa/player';
+import {Button} from '../utils/Button';
+import {getPlayerName} from '@gamepark/croa/CroaOptions';
+
+const WelcomePopup: FunctionComponent<{ player: Player, close: () => void }> = ({ player, close }) => {
+    const {t} = useTranslation()
+    const playerInfo = usePlayer(player.color)
+    return (
+        <div css={[popupOverlayStyle, showPopupOverlayStyle, style]} onClick={close}>
+            <div css={[popupStyle, popupPosition, css`width: 60%`, popupLightStyle]}
+                 onClick={event => event.stopPropagation()}>
+                <div css={closePopupStyle} onClick={close}><FontAwesomeIcon icon={faTimes}/></div>
+                <h2>{t('Welcome {playerName}', { playerName: playerInfo?.name })}</h2>
+                <FrogAnimation css={ frogImage } visible={ true } frog={ player.femaleFrogs.find(f => f.isQueen)! } color={ player.color } animation="blinking" />
+                <FrogAnimation css={ frogImageRight } visible={ true } frog={ player.femaleFrogs.find(f => !f.isQueen)! } color={ player.color } animation="blinking" />
+                <p>{t('You play {color} frogs. Have fun!', {color: getPlayerName(player.color, t) })}</p>
+                <Button css={ welcomeButton } color={ player.color } onClick={close}>{t('OK')}</Button>
+            </div>
+        </div>
+    )
+}
+
+const frogImage = css`
+  position: absolute;
+  bottom: 25%;
+  margin: 0;
+  left: 10%;
+  width: 10%;
+  height: 50%;
+`
+
+const frogImageRight = css`
+  position: absolute;
+  bottom: 25%;
+  right: 13%;
+  margin: 0;
+  left: unset;
+  width: 6%;
+  height: 29%;
+  transform: rotateY(180deg);
+`
+
+const welcomeButton = css`
+  width: 20%;
+  height: 4.5em;
+  margin-left: 40%;
+`;
+
+const style = css`
+  background-color: transparent;
+`;
+
+export default WelcomePopup
