ae23b468d759ede9502c2064595880cac09df785
sulfab <p.beaume@gmail.com>
2021-04-08 15:16:55 +0200

    fix: use shuffled pond before splitting it...

--- rules/src/utils/shuffleSlabs.ts
+++ rules/src/utils/shuffleSlabs.ts
@@ -5,11 +5,11 @@ const shuffleSlabs = (pond: Array<Slab>, dimension: number): Slab[][] => {
   const slabBoard: Slab[][] = [];
   const shuffledSlab = shuffle(pond);
   
-    for (let i = 0; i < shuffledSlab.length; i++) {
-      if (i % dimension === 0) {
-        slabBoard.push(pond.slice(i, i + dimension));
-      }
+  for (let i = 0; i < shuffledSlab.length; i++) {
+    if (i % dimension === 0) {
+      slabBoard.push(shuffledSlab.slice(i, i + dimension));
     }
+  }
     
     return slabBoard;
 }
