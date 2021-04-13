c0466eb0317913a69e7a24d531fbf04a4d815e0e
sulfab <p.beaume@gmail.com>
2021-04-13 13:55:02 +0200

    fix: exchange places tiles with tiles on the shuffled pond

--- app/src/tutorial/CroaTutorial.ts
+++ app/src/tutorial/CroaTutorial.ts
@@ -1,51 +1,61 @@
 import { GameState } from '@gamepark/croa/GameState';
 import { Move, moveFrogMove } from '@gamepark/croa/moves';
 import { initializePlayerBoard, PlayerColor } from '@gamepark/croa/player';
-import { pond, SlabBackType, SlabFrontType } from '@gamepark/croa/pond';
+import { pond, SlabFrontType } from '@gamepark/croa/pond';
 import { shuffleSlabs } from '@gamepark/croa/utils';
 import { TutorialDescription } from '@gamepark/react-client';
+import { Position } from '@gamepark/croa/dist/common/Position';
 
 
-const tilesToPlace: Array<{ front: SlabFrontType, back: SlabBackType, x: number, y: number }> = [
+const tilesToPlace: Array<{ front: SlabFrontType, x: number, y: number }> = [
     // Servant diagonals
-    { front: SlabFrontType.Reed,       back: SlabBackType.Deep, x: 1, y: 2 },
-    { front: SlabFrontType.WaterLily,  back: SlabBackType.Deep, x: 2, y: 3 },
-    { front: SlabFrontType.Mud,        back: SlabBackType.Deep, x: 3, y: 4 },
-    { front: SlabFrontType.Log,        back: SlabBackType.Deep, x: 4, y: 5 },
-    { front: SlabFrontType.Reed,       back: SlabBackType.Deep, x: 5, y: 6 },
-    { front: SlabFrontType.Reed,       back: SlabBackType.Deep, x: 7, y: 6 },
+    { front: SlabFrontType.Reed, x: 1, y: 2 },
+    { front: SlabFrontType.WaterLily, x: 2, y: 3 },
+    { front: SlabFrontType.Mud, x: 3, y: 4 },
+    { front: SlabFrontType.Log, x: 4, y: 5 },
+    { front: SlabFrontType.Reed, x: 5, y: 6 },
+    { front: SlabFrontType.BlueMale, x: 7, y: 6 },
 
     // Queen tiles
-    { front: SlabFrontType.RedMale,    back: SlabBackType.Deep, x: 1, y: 1 },
+    { front: SlabFrontType.RedMale, x: 1, y: 1 },
 
     // Other servants
-    { front: SlabFrontType.Mosquito, back: SlabBackType.Deep, x: 2, y: 1 },
-    { front: SlabFrontType.Mosquito, back: SlabBackType.Deep, x: 3, y: 5 },
-    { front: SlabFrontType.Mosquito, back: SlabBackType.Deep, x: 0, y: 2 },
+    { front: SlabFrontType.Mosquito, x: 2, y: 1 },
+    { front: SlabFrontType.Mosquito, x: 3, y: 5 },
+    { front: SlabFrontType.Mosquito, x: 0, y: 2 },
 
     // Pike on opponent
-    { front: SlabFrontType.Pike,     back: SlabBackType.Deep, x: 7, y: 5 }
+    { front: SlabFrontType.Pike, x: 7, y: 5 }
 ]
 
 
-const shuffledPond = shuffleSlabs(pond, 8).map((row, x) => row.map((tile, y) => {
-    const newTile = tilesToPlace.find(tile => tile.x === x && tile.y === y);
-    if (newTile) {
-        return {
-            front: newTile.front,
-            back: newTile.back
-        }
+const shuffledPond = shuffleSlabs(pond, 8);
+
+tilesToPlace.forEach(tile => {
+    let replacingPosition: Position | undefined;
+    shuffledPond.forEach((row, rowIndex) =>
+      row.forEach((t, columnIndex) => {
+          if (t.front === tile.front && !tilesToPlace.some(tileToPlace => tileToPlace.x === rowIndex && tileToPlace.y === columnIndex)) {
+              replacingPosition = {x: rowIndex, y: columnIndex};
+          }
+      })
+    );
+
+    if (!replacingPosition) {
+        throw new Error("Impossible to build the tutorial");
     }
 
-    return tile;
-}));
+    const tempTile = shuffledPond[tile.x][tile.y];
+    shuffledPond[tile.x][tile.y] =  shuffledPond[replacingPosition.x][replacingPosition.y];
+    shuffledPond[replacingPosition.x][replacingPosition.y] = tempTile;
+});
 
 const CroaTutorial: TutorialDescription<GameState, Move, PlayerColor> = {
 
     setupTutorial: () => [
         {
             players: [PlayerColor.Blue, PlayerColor.Pink].map((p, index) => initializePlayerBoard(2, index, p)), 
-            pond: shuffledPond, 
+            pond: shuffledPond,
             activePlayer: PlayerColor.Blue
         }, [PlayerColor.Blue, PlayerColor.Pink]],
     expectedMoves: () => [
